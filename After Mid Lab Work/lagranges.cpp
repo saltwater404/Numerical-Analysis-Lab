70b8b507a8b4c5d8ef6ff4a65095020c91d11dd8
Faisal Ahmed <45968146+saltwater404@users.noreply.github.com>
2020-03-01 09:33:57 +0600

    Add files via upload

---
+++ After Mid Lab Work/lagranges.cpp
@@ -0,0 +1,40 @@
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+struct Data 
+{ 
+	int x, y; 
+}; 
+
+double interpolate(Data f[], int xi, int n) 
+{ 
+	double result = 0;
+
+	for (int i=0; i<n; i++) 
+	{ 
+		
+		double term = f[i].y; 
+		for (int j=0;j<n;j++) 
+		{ 
+			if (j!=i) 
+				term = term*(xi - f[j].x)/double(f[i].x - f[j].x); 
+		} 
+
+		
+		result += term; 
+	} 
+
+	return result; 
+} 
+
+
+int main() 
+{ 
+
+	Data f[] = {{5,12}, {6,13}, {9,14}, {11,16}}; 
+
+	cout << "Value of f(10) is : " << interpolate(f, 10, 10); 
+
+	return 0; 
+} 
